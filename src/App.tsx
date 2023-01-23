cf807f11d336a3ab646e07e26b3863d148f990a5
tony dugué <duguetony@gmail.com>
2023-01-23 23:28:32 +0100

    Added Sales/Daily & Sales/Monthly pages content with Nivo Line chart

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Customers from "./scenes/customers";
 import Transactions from "./scenes/transactions";
 import Geography from "./scenes/geography";
 import Overview from "./scenes/overview";
+import Daily from "./scenes/daily";
+import Monthly from "./scenes/monthly";
 
 const App: FunctionComponent = () => {
   const mode = useAppSelector((state) => state.global.mode);
@@ -30,6 +32,8 @@ const App: FunctionComponent = () => {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
